986e29e11ae6cb5d828c2dc0636e3b0efbfa87e2
s533294 <89223971+s533294@users.noreply.github.com>
2023-04-14 12:24:01 -0500

    Update car.js

--- controllers/car.js
+++ controllers/car.js
@@ -79,3 +79,21 @@ exports.car_detail = async function(req, res) {
         res.send(`{"error": document for id ${req.params.id} not found`);
     }
    };
+exports.costume_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Costume.findById( req.params.id)
+        // Do updates of properties
+        if(req.body.costume_type)
+        toUpdate.costume_type = req.body.costume_type;
+        if(req.body.cost) toUpdate.cost = req.body.cost;
+        if(req.body.size) toUpdate.size = req.body.size;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
+   };
