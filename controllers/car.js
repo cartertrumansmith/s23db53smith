1392eb39a475b7788e79b7a91d4aad6db17a5149
s533294 <89223971+s533294@users.noreply.github.com>
2023-04-13 21:17:15 -0500

    Part 7

--- controllers/car.js
+++ controllers/car.js
@@ -44,3 +44,26 @@ exports.car_view_all_Page = async function(req, res){
         res.send(`{"error": ${err}}`);
     }
    };
+// Handle Costume create on POST.
+exports.car_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Car();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.color = req.body.color;
+    document.make = req.body.make;
+    document.model = req.body.model;
+    document.style = req.body.style;
+    document.price = req.body.price;
+
+    try{
+        let result = await document.save();
+        res.send(result);
+    }
+    catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
