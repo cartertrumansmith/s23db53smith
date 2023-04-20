f3508e1a80c1c501b3e2dee319fc72b1afb31436
s533294 <89223971+s533294@users.noreply.github.com>
2023-04-19 20:49:21 -0500

    Update car.js

--- controllers/car.js
+++ controllers/car.js
@@ -79,21 +79,34 @@ exports.car_detail = async function(req, res) {
         res.send(`{"error": document for id ${req.params.id} not found`);
     }
    };
-exports.costume_update_put = async function(req, res) {
+exports.car_update_put = async function(req, res) {
     console.log(`update on id ${req.params.id} with body
     ${JSON.stringify(req.body)}`)
     try {
-        let toUpdate = await Costume.findById( req.params.id)
+        let toUpdate = await Car.findById( req.params.id)
         // Do updates of properties
-        if(req.body.costume_type)
-        toUpdate.costume_type = req.body.costume_type;
-        if(req.body.cost) toUpdate.cost = req.body.cost;
-        if(req.body.size) toUpdate.size = req.body.size;
+        if(req.body.color) toUpdate.color = req.body.color;
+        if(req.body.make) toUpdate.make = req.body.make;
+        if(req.body.model) toUpdate.model = req.body.model;
+        if(req.body.style) toUpdate.style = req.body.style;
+        if(req.body.price) toUpdate.price = req.body.price;
         let result = await toUpdate.save();
-        console.log("Sucess " + result)
+        console.log("Success " + result)
         res.send(result)
     } catch (err) {
         res.status(500)
         res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
+   };
+   // Handle Car delete on DELETE.
+exports.car_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Car.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
     }
    };
